2a470de88774acf7e107fa65f74f0ff8d801e1e3
TannerCox36 <tcox6@outlook.com>
2023-04-26 17:03:52 -0400

    Add Lists of big market key words

---
+++ BigMarkets.py
@@ -0,0 +1,13 @@
+teams = ['New York Yankees', 'New York Mets', 'New York Giants', 'New York Jets', 'Brooklyn Nets', 'New York Knicks', 'New York Rangers', 'New York Islanders',
+        'Los Angeles Dodgers', 'Los Angeles Angels', 'Los Angeles Lakers', 'Los Angeles Clippers', 'Los Angeles Rams', 'Los Angeles Chargers', 'Los Angeles Kings', 'Anaheim Ducks',
+        'Chicago Cubs', 'Chicago White Sox', 'Chicago Bulls', 'Chicago Bears', 'Chicago Blackhawks',
+        'Philadelphia Phillies', 'Philadelphia Eagles', 'Philadelphia 76ers', 'Philadelphia Flyers',
+        'Dallas Cowboys', 'Dallas Mavericks', 'Dallas Stars', 'Texas Rangers']
+
+cities = ['New York City', 'Los Angeles', 'Chicago', 'Philadelphia', 'Dallas-Ft. Worth', 'Texas', 'Anaheim']
+
+city_teams = {'New York City': ['Yankees', 'Mets', 'Giants', 'Jets', 'Nets', 'Knicks', 'Rangers', 'Islanders'],
+              'Los Angeles': ['Dodgers', 'Angels', 'Lakers', 'Clippers', 'Rams', 'Chargers', 'Kings', 'Ducks'],
+              'Chicago': ['Cubs', 'White Sox', 'Bulls', 'Bears', 'Blackhawks'],
+              'Philadelphia': ['Phillies', 'Eagles', '76ers', 'Flyers'],
+              'Dallas-Ft. Worth': ['Cowboys', 'Mavericks', 'Stars', 'Rangers']}
